df4111b4b10707c3dea2ab3c9c83b817aef638e4
SHAIK AFREED <skafrid482@gmail.com>
2025-07-16 11:15:55 +0530

    imgs, icon

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,9 +1,11 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
-import img1 from "../assets/img1.jpg";
-import img2 from "../assets/img2.jpg";
-import img3 from "../assets/img3.jpg";
+
+const img1 = "https://i.ibb.co/5XYHT0Gv/image1.jpg";
+const img2 = "https://i.ibb.co/9kqZYTf/image2.jpg";
+const img3 = "https://i.ibb.co/B5LZndH/image3.jpg";
+
 const sampleImages = [img1, img2, img3];
 
 export default function Carousel() {
